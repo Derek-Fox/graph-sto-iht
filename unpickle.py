30962e6cbf6864e814bfd013ca4baec772c82214
Derek Fox <defox@uncg.edu>
2024-06-04 16:25:48 -0400

    trying to implement graph-svrg-iht

--- unpickle.py
+++ unpickle.py
@@ -4,13 +4,14 @@ import sys
 def unpickle(f):
     with open(f, "rb") as picklefile:
         data = pickle.load(picklefile)
-        print(data)
+        return data
 
 def main():
     if len(sys.argv) !=2:
         print("Usage: python unpickle.py <picklefile>")
     file = sys.argv[1]
-    unpickle(file)
+    data = unpickle(file)
+    print(data)
 
 if __name__ == '__main__':
     main()
